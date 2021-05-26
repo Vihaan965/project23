406475e287fbe91a44fde129e09b9e77e1aeae53
Vihaan965 <84268416+Vihaan965@users.noreply.github.com>
2021-05-26 15:37:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function changecolor() {
+    document.getElementById("Darkred").style.color = "blue";
+    document.body.style.backgroundColor = "white";
+}
+
+function change1color() {
+    document.getElementById("Darkred").style.color = "aqua";
+    document.body.style.backgroundColor = "black";
+}
+
+function change2color() {
+    document.getElementById("Darkred").style.color = "aqua";
+    document.body.style.backgroundColor = "darkblue";
+}
